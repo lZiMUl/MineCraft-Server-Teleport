39ad50da54295c6141fb42eefc56e61da82940d5
lZiMUl <AA2908554069@gmail.com>
2022-11-23 16:43:31 +0800

    new Project

--- scripts/index.js
+++
@@ -1,9 +0,0 @@
-import { world } from '@minecraft/server';
-import Menu from './menu';
-world.events.itemUse.subscribe(({ item, source }) => {
-    switch (item.typeId) {
-        case 'minecraft:compass':
-            new Menu(source);
-            break;
-    }
-});
